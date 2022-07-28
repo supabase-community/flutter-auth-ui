0d8f55bd88852b9d32db912bda3e64be780c358c
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-07-28 15:26:48 +0300

    update export list in lib

--- lib/supa_flutter_auth.dart
+++ lib/supa_flutter_auth.dart
@@ -3,3 +3,5 @@ library supa_flutter_auth;
 export 'src/utils/supabase_auth.dart';
 export 'src/screens/supa_email_auth.dart';
 export 'src/screens/supa_magic_auth.dart';
+export 'src/screens/supa_reset_password.dart';
+export 'src/screens/supa_send_email.dart';
