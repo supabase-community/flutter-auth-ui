712d281f8e6020a4f4c5d247d79a953a320b34cc
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-07-30 17:29:44 +0300

    changed name to avoid clashes with supabase_flutter

--- lib/supa_flutter_auth.dart
+++ lib/supa_flutter_auth.dart
@@ -1,6 +1,6 @@
 library supa_flutter_auth;
 
-export 'src/utils/supabase_auth.dart';
+export 'src/utils/supabase_auth_ui.dart';
 export 'src/screens/supa_email_auth.dart';
 export 'src/screens/supa_magic_auth.dart';
 export 'src/screens/supa_reset_password.dart';
