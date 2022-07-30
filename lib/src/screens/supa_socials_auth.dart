50ebd07fd47e74651e960693e1ee46b0f51b9f2f
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-07-30 17:31:21 +0300

    add logic, ui to socials auth in lib

--- lib/src/screens/supa_socials_auth.dart
+++ lib/src/screens/supa_socials_auth.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../utils/supabase_auth_ui.dart';
 
-enum SocialProviders { google, twitter }
+enum SocialProviders {
+  apple,
+  azure,
+  bitbucket,
+  discord,
+  facebook,
+  github,
+  gitlab,
+  google,
+  slack,
+  spotify,
+  twitch,
+  twitter
+}
 
 class SupaSocialsAuth extends StatefulWidget {
   final List<SocialProviders> socialProviders;
@@ -20,35 +35,41 @@ class _SupaSocialsAuthState extends State<SupaSocialsAuth> {
   @override
   Widget build(BuildContext context) {
     final coloredBg = widget.colored == true;
+    final emptyList = widget.socialProviders.isEmpty;
     return Expanded(
-      child: ListView.builder(
-        itemCount: widget.socialProviders.length,
-        itemBuilder: (context, index) => Container(
-          margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-          child: OutlinedButton.icon(
-            icon: Icon(
-              Icons.youtube_searched_for,
-              color: coloredBg ? Colors.white : null,
-            ),
-            style: ButtonStyle(
-              minimumSize: MaterialStateProperty.all(
-                const Size(double.infinity, 0),
+      child: emptyList
+          ? ErrorWidget(Exception('Social provider list cannot be empty'))
+          : ListView.builder(
+              itemCount: widget.socialProviders.length,
+              itemBuilder: (context, index) => Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                child: OutlinedButton.icon(
+                  icon: Icon(
+                    Icons.youtube_searched_for,
+                    color: coloredBg ? Colors.white : null,
+                  ),
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(
+                      const Size(double.infinity, 0),
+                    ),
+                    padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(15)),
+                    backgroundColor: MaterialStateProperty.all(
+                        coloredBg ? Colors.redAccent : null),
+                  ),
+                  onPressed: () {
+                    SupabaseAuthUi().socialSignIn(widget.socialProviders[index].toString());
+                  },
+                  label: Text(
+                    widget.socialProviders[index].toString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: coloredBg ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ),
               ),
-              padding: MaterialStateProperty.all(const EdgeInsets.all(15)),
-              backgroundColor: MaterialStateProperty.all(
-                  coloredBg ? Colors.redAccent : null),
             ),
-            onPressed: () {},
-            label: Text(
-              widget.socialProviders[index].toString(),
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: coloredBg ? Colors.white : Colors.black,
-              ),
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
