aad8f15b0186fd65edd6f02a8c196ca679469b7e
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-08-05 15:39:58 +0300

    update constants file in example

--- example/lib/constants.dart
+++ example/lib/constants.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 
 final appBar = AppBar(
-        title: const Text('auth ui example'),
-        automaticallyImplyLeading: false,
-      );
+  title: const Text('auth ui example'),
+  automaticallyImplyLeading: false,
+);
+
+const optionText = Text(
+  'Or',
+  style: TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  ),
+  textAlign: TextAlign.center,
+);
+
+const spacer = SizedBox(
+  height: 12,
+);
+
+dynamic magicSignInBtn(BuildContext context) {
+    return ElevatedButton(
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all(
+        const EdgeInsets.all(18),
+      ),
+    ),
+    child: const Text(
+      'Sign In with Magic Link',
+      style: TextStyle(fontWeight: FontWeight.bold),
+    ),
+    onPressed: () {
+      Navigator.pushNamed(context, '/magic_link');
+    },
+  );
+}
