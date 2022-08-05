aaa8336635c4742495d23d3e2cf4aab4fef2656d
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-08-05 14:58:51 +0300

    clean up home and main, add logout btn

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -2,29 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
 class Home extends StatelessWidget {
-  Home({Key? key}) : super(key: key);
-
-  final user = SupabaseAuthUi().getActiveUser();
-
-  void checkUser(context) {
-    if (user == null) Navigator.popAndPushNamed(context, '/');
-  }
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    /** 
-     * figure something better - avoid setState() or markNeedsBuild() called during build Error
-     * maybe use provider?
-     * or implement that with go_router redirection check
-    */
-    Future.delayed(Duration.zero, () {
-      checkUser(context);
-    });
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          'You are home',
-          style: TextStyle(fontSize: 42),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You are home',
+              style: TextStyle(fontSize: 42),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                SupabaseAuthUi().signOutActiveUser();
+                Navigator.of(context).popUntil(ModalRoute.withName('/'));
+              },
+              child: const Text(
+                'LogOut',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
         ),
       ),
     );
