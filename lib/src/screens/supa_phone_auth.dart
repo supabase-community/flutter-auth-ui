e26efe9a4e6e9ba87d4b12a916c8b8a61bc8c330
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-08-10 18:31:07 +0300

    add phone logic in lib

---
+++ lib/src/screens/supa_phone_auth.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_auth_ui/src/utils/constants.dart';
+import 'package:supabase_auth_ui/src/utils/supabase_auth_ui.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupaPhoneAuth extends StatefulWidget {
+  final String redirectUrl;
+
+  const SupaPhoneAuth({Key? key, required this.redirectUrl}) : super(key: key);
+
+  @override
+  State<SupaPhoneAuth> createState() => _SupaPhoneAuthState();
+}
+
+class _SupaPhoneAuthState extends State<SupaPhoneAuth> {
+  final _formKey = GlobalKey<FormState>();
+  final _phone = TextEditingController();
+  final _password = TextEditingController();
+
+  SupabaseAuthUi supaAuth = SupabaseAuthUi();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _phone.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a valid email address';
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.phone),
+              hintText: 'Enter your phone number',
+            ),
+            controller: _phone,
+          ),
+          spacer(16),
+          TextFormField(
+            validator: (value) {
+              if (value == null || value.isEmpty || value.length < 6) {
+                return 'Please enter a password that is atleast 6 characters long';
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.lock),
+              hintText: 'Enter your password',
+            ),
+            obscureText: true,
+            controller: _password,
+          ),
+          spacer(16),
+          ElevatedButton(
+            child: const Text(
+              'Sign Up',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onPressed: () async {
+              if (!_formKey.currentState!.validate()) {
+                return;
+              }
+
+              try {
+                await supaAuth.signInUserWithPhone(_phone.text, _password.text);
+                successAlert;
+                if (!mounted) return;
+                Navigator.popAndPushNamed(context, widget.redirectUrl, arguments: {"phone": _phone.text});
+              } on GoTrueException catch (error) {
+                await warningAlert(context, error.message);
+              } catch (error) {
+                await warningAlert(context, 'Unexpected error has occured');
+              }
+
+              setState(() {
+                _phone.text = '';
+                _password.text = '';
+              });
+            },
+          ),
+          spacer(10),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/supa_verify_phone.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_auth_ui/src/utils/supabase_auth_ui.dart';
+import 'package:supabase_auth_ui/src/utils/constants.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupaVerifyPhone extends StatefulWidget {
+  final String? redirectUrl;
+
+  const SupaVerifyPhone({Key? key, this.redirectUrl}) : super(key: key);
+
+  @override
+  State<SupaVerifyPhone> createState() => _SupaVerifyPhoneState();
+}
+
+class _SupaVerifyPhoneState extends State<SupaVerifyPhone> {
+  Map? data;
+  final _formKey = GlobalKey<FormState>();
+  final _code = TextEditingController();
+
+  SupabaseAuthUi supaAuth = SupabaseAuthUi();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _code.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var args = ModalRoute.of(context)?.settings.arguments;
+    if (args != null) data = args as Map;
+    print(data);
+    return Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            validator: (value) {
+              if (value == null ||
+                  value.isEmpty ) {
+                return 'Please enter a valid email address';
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.code),
+              hintText: 'Enter the code sent',
+            ),
+            controller: _code,
+          ),
+          spacer(16),
+          ElevatedButton(
+            child: const Text(
+              'Verify Phone',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onPressed: () async {
+              if (!_formKey.currentState!.validate()) {
+                return;
+              }
+              try {
+                await supaAuth.verifyPhoneUser(data!["phone"], _code.text);
+                successAlert;
+                if (!mounted) return;
+                Navigator.popAndPushNamed(context, widget.redirectUrl ?? '/');
+              } on GoTrueException catch (error) {
+                await warningAlert(context, error.message);
+              } catch (error) {
+                await warningAlert(context, 'Unexpected error has occured');
+              }
+              setState(() {
+                _code.text = '';
+              });
+            },
+          ),
+          spacer(10),
+        ],
+      ),
+    );
+  }
+}
