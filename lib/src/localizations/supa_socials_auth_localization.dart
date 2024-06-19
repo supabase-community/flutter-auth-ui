a14ded131d0c579e0dc49e895aa8ec625b57c65f
dshukertjr <dshukertjr@gmail.com>
2024-06-19 17:16:40 +0900

    feat: Allow the OAuth provider labels to be overridden on SocialLogin component

--- lib/src/localizations/supa_socials_auth_localization.dart
+++ lib/src/localizations/supa_socials_auth_localization.dart
@@ -1,13 +1,33 @@
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+
 class SupaSocialsAuthLocalization {
   final String updatePassword;
   final String successSignInMessage;
   final String unexpectedError;
   final String continueWith;
 
+  /// Overrides the name of the OAuth provider shown on the sign-in button.
+  ///
+  /// ```dart
+  /// SupaSocialsAuth(
+  ///   socialProviders: const [OAuthProvider.azure],
+  ///   localization: const SupaSocialsAuthLocalization(
+  ///     oAuthProviderLabelOverrides: {
+  ///       OAuthProvider.azure: 'Microsoft (Azure)'
+  ///     },
+  ///   ),
+  ///   onSuccess: (session) {
+  ///     // sHandle success
+  ///   },
+  /// ),
+  /// ```
+  final Map<OAuthProvider, String> oAuthProviderLabelOverrides;
+
   const SupaSocialsAuthLocalization({
     this.updatePassword = 'Update Password',
     this.successSignInMessage = 'Successfully signed in!',
     this.unexpectedError = 'An unexpected error occurred',
     this.continueWith = 'Continue with',
+    this.oAuthProviderLabelOverrides = const {},
   });
 }
