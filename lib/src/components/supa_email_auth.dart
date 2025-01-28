4d8a1a80469552906f8bc4e7dde4e25f01dfbe12
Savva <74972418+castletaste@users.noreply.github.com>
2025-01-28 14:13:45 +0700

    Update autofocus in supa_email_auth.dart

--- lib/src/components/supa_email_auth.dart
+++ lib/src/components/supa_email_auth.dart
@@ -298,7 +298,7 @@ class _SupaEmailAuthState extends State<SupaEmailAuth> {
               keyboardType: TextInputType.emailAddress,
               autofillHints: const [AutofillHints.email],
               autovalidateMode: AutovalidateMode.onUserInteraction,
-              autofocus: true,
+              autofocus: false,
               focusNode: _emailFocusNode,
               textInputAction: _isRecoveringPassword
                   ? TextInputAction.done
