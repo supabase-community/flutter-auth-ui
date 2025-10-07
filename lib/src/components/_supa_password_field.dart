30f2df63e146f0dd5e90e6792a7dd222843bc96b
FatumaA <67555014+FatumaA@users.noreply.github.com>
2025-10-07 17:31:50 +0300

    feat: create shared password component with toggle

---
+++ lib/src/components/_supa_password_field.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+/// Internal password field component with visibility toggle
+class SupaPasswordField extends StatefulWidget {
+  /// Controller for the password field
+  final TextEditingController controller;
+
+  /// Validator function for the password field
+  final String? Function(String?)? validator;
+
+  /// Label text for the password field
+  final String labelText;
+
+  /// Prefix icon for the password field
+  final Widget? prefixIcon;
+
+  /// Autofill hints for the password field
+  final Iterable<String>? autofillHints;
+
+  /// Text input action for the password field
+  final TextInputAction? textInputAction;
+
+  /// Callback when field is submitted
+  final void Function(String)? onFieldSubmitted;
+
+  /// Whether the field should auto-validate
+  final AutovalidateMode? autovalidateMode;
+
+  const SupaPasswordField({
+    super.key,
+    required this.controller,
+    this.validator,
+    required this.labelText,
+    this.prefixIcon,
+    this.autofillHints,
+    this.textInputAction,
+    this.onFieldSubmitted,
+    this.autovalidateMode,
+  });
+
+  @override
+  State<SupaPasswordField> createState() => _SupaPasswordFieldState();
+}
+
+class _SupaPasswordFieldState extends State<SupaPasswordField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      validator: widget.validator,
+      obscureText: _obscureText,
+      autofillHints: widget.autofillHints,
+      textInputAction: widget.textInputAction,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      autovalidateMode: widget.autovalidateMode,
+      decoration: InputDecoration(
+        prefixIcon: widget.prefixIcon,
+        label: Text(widget.labelText),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _obscureText ? Icons.visibility_off : Icons.visibility,
+          ),
+          onPressed: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
